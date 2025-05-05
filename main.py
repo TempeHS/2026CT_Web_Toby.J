4f973b45b285d462caec24f0504f701d4aa3c686
Toby J <toby.johnson19@education.nsw.gov.au>
2025-05-05 02:49:16 +0000

    started website and menu

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/index.html')
 @app.route('/')
-
-def hello_world():
-    backend_data = "Scissors, Paper, Rock!"
-    return render_template("index.html",front_end_data = backend_data ), 200
+def index():
+    return render_template("index.html"), 200
 
 
 if __name__ == '__main__':
