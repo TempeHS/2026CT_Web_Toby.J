0e4e9f81b169bc1e78a09da792a5ff7626ba323e
Toby J <toby.johnson19@education.nsw.gov.au>
2025-05-25 23:36:21 +0000

    updated cards

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
     card_data = (
         ("What skill level is this for?", "Learn Python website is for beginners to pros! There are lessons for all skill levels and experience", "Take the skill level quiz!", "static/images/card1use.svg"),
         ("What is Python used for?", "Python can be used to build anything you can imagine, from games to lessons to websites!", "Learn what you can do with Python today", "static/images/card2what.svg"),
-        ("How can you learn?", "Random text", "Link", "static/images/card3how.svg"),
+        ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card3how.svg"),
     )
     return render_template("index.html", cards=card_data), 200
 
