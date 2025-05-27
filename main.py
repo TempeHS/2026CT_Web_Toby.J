5a89183048ae1b0b977e86a8b2e62b6611b918c8
Toby J <toby.johnson19@education.nsw.gov.au>
2025-05-27 23:39:13 +0000

    updated grid patterns

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("What skill level is this for?", "Learn Python website is for beginners to pros! There are lessons for all skill levels and experience", "Take the skill level quiz!", "static/images/card1use.svg"),
+        ("What skill level is this for?", "Learn Python website is for beginners to pros! There are lessons for all skill levels and experience", "Learn Python for all skill levels today!", "static/images/card1use.svg"),
         ("What is Python used for?", "Python can be used to build anything you can imagine, from games to lessons to websites!", "Learn what you can do with Python today", "static/images/card2what.svg"),
         ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card3how.svg"),
     )
