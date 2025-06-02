27731053650c110f71c9d8586e513fe427cbbdcd
Toby J <toby.johnson19@education.nsw.gov.au>
2025-06-02 02:49:15 +0000

    updated cards, added signup page

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ def index():
         ("What skill level is this for?", "Learn Python website is for beginners to pros! There are lessons for all skill levels and experience", "Learn Python for all skill levels today!", "static/images/card1use.svg"),
         ("What is Python used for?", "Python can be used to build anything you can imagine, from games to lessons to websites!", "Learn what you can do with Python today", "static/images/card2what.svg"),
         ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card3how.svg"),
+        ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card4beginner.svg"),
+        ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card5intermediate.svg"),
+        ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card6advanced.svg"),
     )
     return render_template("index.html", cards=card_data), 200
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html"), 200
+
+@app.route('/signup.html')
+def signup():
+    return render_template("signup.html"), 200
 
 
 if __name__ == '__main__':
