52bfb67b3f3c611d9296603f27ed56c367cba535
Toby J <toby.johnson19@education.nsw.gov.au>
2025-06-16 02:49:24 +0000

    updated home lessons and improved card images

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def index():
         ("What skill level is this for?", "Learn Python website is for beginners to pros! There are lessons for all skill levels and experience", "Learn Python for all skill levels today!", "static/images/card1use.svg"),
         ("What is Python used for?", "Python can be used to build anything you can imagine, from games to lessons to websites!", "Learn what you can do with Python today", "static/images/card2what.svg"),
         ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card3how.svg"),
-        ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card4beginner.svg"),
-        ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card5intermediate.svg"),
-        ("How can you learn?", "You can learn through games, interactive lessons and quizzes", "Learn python the fun way today!", "static/images/card6advanced.svg"),
+        ("Learn Python Syntax and Basic Variables", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ullamcorper lorem id venenatis condimentum. Nam ultricies nibh pharetra felis pretium tincidunt.", "Learn python the fun way today!", "static/images/card4beginner.svg"),
+        ("Learn Booleans, Operators and Conditionals", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ullamcorper lorem id venenatis condimentum. Nam ultricies nibh pharetra felis pretium tincidunt. ", "Learn python the fun way today!", "static/images/card5intermediate.svg"),
+        ("Learn For/While loops, Tuples, Lists and F-strings", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ullamcorper lorem id venenatis condimentum. Nam ultricies nibh pharetra felis pretium tincidunt. .", "Learn python the fun way today!", "static/images/card6advanced.svg"),
     )
     return render_template("index.html", cards=card_data), 200
 
